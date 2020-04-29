9bb7e31007d848bf00ddb464f2b7355f784c8dd6
ctrumbo <ctrumbo@uncc.edu>
2020-04-29 18:50:37 +0000

    Added very basic forum functionality

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -1,4 +1,24 @@
 class ForumController < ApplicationController
+    def index
+        @forum = Forum.all
+    end
+    
+    def show
+        @forum = Forum.find(params[:id])
+    end
+    
     def forum
     end
+    
+    def create
+    @forum = Forum.new(params[:forum])
+    
+    @forum.save
+    redirect_to @forum
+    end
 end
+
+private
+    def forum_params
+        params.require(:forum).permit(:title, :text)
+    end
