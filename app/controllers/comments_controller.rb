22168fc71731273fbb13153ac1c31bb44e0e4a8d
ctrumbo <ctrumbo@uncc.edu>
2020-04-30 17:11:46 +0000

    Added comment deletion functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
         redirect_to forum_path(@forum)
     end
     
+    def destroy
+        @forum = Forum.find(params[:forum_id])
+        @comment = @forum.comments.find(params[:id])
+        @comment.destroy
+        redirect_to forum_path(@forum)
+    end
+    
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body)
